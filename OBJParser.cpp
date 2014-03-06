857106ef031929d800c54646775c583330f83ba7
Miles Lindheimer <mileslindheimer@gmail.com>
2014-03-06 01:36:26 -0800

    outputting images to ppm file, using objparser as testing file

--- OBJParser.cpp
+++ OBJParser.cpp
@@ -4,6 +4,10 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include "Color.h"
+
+#define width 100
+#define height 100
 
 using namespace std;
 
@@ -11,34 +15,49 @@ class OBJParser {
     
 };
 
-void log(char* c) {}
-
+unsigned char foo[] = {(unsigned char)1,(unsigned char)0,(unsigned char)0};
 
 int main(int argc, char** args){
-    char* token = 0;
-    //start writing to trace file
-    ofstream trace("tmp/trace.txt");
-    for (int i=1; i<argc; i++) {
-        ifstream file;
-        file.open((string) "obj/" + (string) args[i]);
-        if (!file.good()){
-            cout << "bad file\n";
-            trace << "bad file\n";
-        }
-        else {
-            trace << "opening " << args[i] << "\n";
-            char buffer[30];
-            string token;
-            while (file >> token) {
-                //token = strtok(buffer, " ");
-                cout << token << "\n";
-                trace << token << "\n";
-            }
-            file.close();
+    unsigned char r,g,b;
+    Color image[width][height];
+    FILE *output = fopen("output.ppm", "wb");
+    fprintf(output, "P6\n%d %d\n255\n", width, height);
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            r = (unsigned char) j%255;
+            g = (unsigned char) i%255;
+            b = (unsigned char) j%255;
+            fprintf(output, "%c%c%c", r,g,b );
         }
     }
-    //close trace file
-    trace << "end trace\n";
-    trace.close();
+    fclose(output);
+    
+    
+    
+//    char* token = 0;
+//    //start writing to trace file
+//    ofstream trace("tmp/trace.txt");
+//    for (int i=1; i<argc; i++) {
+//        ifstream file;
+//        file.open((string) "obj/" + (string) args[i]);
+//        if (!file.good()){
+//            cout << "bad file\n";
+//            trace << "bad file\n";
+//        }
+//        else {
+//            trace << "opening " << args[i] << "\n";
+//            char buffer[30];
+//            string token;
+//            while (file >> token) {
+//                //token = strtok(buffer, " ");
+//                cout << token << "\n";
+//                trace << token << "\n";
+//            }
+//            file.close();
+//        }
+//    }
+//    //close trace file
+//    trace << "end trace\n";
+//    trace.close();
     return 0;
 }
