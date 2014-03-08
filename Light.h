a8f1e9ef57bcb22e207c93f426d64e051712ad6e
Miles Lindheimer <mileslindheimer@gmail.com>
2014-03-07 17:57:05 -0800

    abstract light class

---
+++ Light.h
@@ -0,0 +1,22 @@
+Ï
+
+#ifndef Light_H
+#define Light_H
+
+//#include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include "LocalGeo.h"
+#include "Ray.h"
+#include "Color.h"
+
+
+class Light {
+    
+public:
+    virtual void generateLightRay(LocalGeo& local, Ray* lray, Color* lcolor);
+
+
+};
+
+#endif /* Light_H */
