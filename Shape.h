3b1c0d4de654b09034b153f5bef4c1069a584b04
jesca116@berkeley.edu <jesca116@berkeley.edu>
2014-03-09 23:08:37 -0700

    fixed classes for shape

--- Shape.h
+++ Shape.h
@@ -4,13 +4,32 @@ class Shape
 public:
     bool intersect(Ray& ray, float* thit, LocalGeo* local);
     bool intersectP(Ray& ray);
+ 
+
+};
+
+
+class Sphere : public Shape
+{
+public:
     void createSphere(float centerx,float centery, centerz, float radius, float tmin, float tmax);
-    void createTriangle(float centerx,float centery, float radius); //fix later
-    bool isTriangle();
-    bool isCircle();
-    
+    bool intersect(Ray& ray, float* thit, LocalGeo* local);
+    bool intersectP(Ray& ray);
 private:
     float tmin, tmax, radius;
     Vector3f centervec;
 
+    
 };
+
+class Triangle : public Shape
+{
+public:
+    void createTriangle(float centerx,float centery, float radius); //fix later
+	bool intersect(Ray& ray, double* thit, LocalGeo* local);
+	bool intersectP(Ray& ray);
+    
+    
+};
+
+#endif
