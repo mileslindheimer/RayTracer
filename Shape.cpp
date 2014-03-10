3e538b9129d91dffdf4ea4e25fd2ffc957a47da0
jesca116@berkeley.edu <jesca116@berkeley.edu>
2014-03-09 23:41:32 -0700

    intersect discriminant

--- Shape.cpp
+++ Shape.cpp
@@ -14,20 +14,29 @@ bool Sphere::intersectP(Ray &ray){
     /*If the discriminant is positive, there are two solutions: one solution where
      the ray enters the sphere and one where it leaves. If the discriminant is zero, the
      ray grazes the sphere touching it at exactly one point.*/
-    Point e=Ray.pos();
-    float A=
-    float discriminant = //something
+    Vector3f pos=ray.posvec();
+    Vector3f dir = ray.dir();
+    float center = this->center;
+    float radius = this->radius;
+    float a = dir * dir;
+    float b= 2 * ray.dir() * (ray.posvec() - center);
+    float c = (ray.posvec()-center) * ((ray.posvec()-center)-pow(radius,2.0));
+    
+    
+    float discriminant = pow(b,2.0)-(4*a*c);
+    
     
     if(discrim < 0){
 		return false;
 	}
     
 	else{
-        return false; //do something else
+        return false; //do something else, calculate t min and t max
         }
 }
+ 
+ 
 
 bool Sphere::intersect(Ray &ray, double *thit, LocalGeo *local){
 	
 }
- 
